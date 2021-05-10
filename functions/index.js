12892a0f72d1c77b12912a9a58403addaa2d003e
erezrokah <erezrokah@users.noreply.github.com>
2021-05-10 19:07:08 +0300

    chore: reproduce issue

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,10 @@
 exports.handler = async () => {
-  return { statusCode: 200, body: 'index.js' };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      URL: process.env.URL,
+      DEPLOY_URL: process.env.DEPLOY_URL,
+      DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL,
+    }),
+  };
 };
