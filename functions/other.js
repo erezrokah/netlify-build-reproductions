dc28dbb1edf9ae1961bba438c7236f874067cf65
erezrokah <erezrokah@users.noreply.github.com>
2021-08-20 14:53:24 +0200

    chore: reproduce issue

--- functions/other.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  console.log({ NODE_ENV: process.env.NODE_ENV });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ NODE_ENV: process.env.NODE_ENV }),
-  };
-};
